9b7cfd5e34dad93bba03f1ff976176ea892f4a8c
Alisamar Husain <zrthxn@gmail.com>
2019-07-22 01:14:43 +0530

    Firebase creds

--- libs/database/src/lib/firebase.ts
+++ libs/database/src/lib/firebase.ts
@@ -1,3 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA1bjlmNhY__t7MPeaSafZfeuOVQ5mF_jQ",
+  authDomain: "clubgo-dev.firebaseapp.com",
+  databaseURL: "https://clubgo-dev.firebaseio.com",
+  projectId: "clubgo-dev",
+  storageBucket: "clubgo-dev.appspot.com",
+  messagingSenderId: "1061379349751",
+  appId: "1:1061379349751:web:a10bf20da593f36d"
+};
+
 export class Firebase {
   
 }
