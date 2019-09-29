dd027d21aec405466d11618a3eb2abe670b54a69
Alisamar Husain <zrthxn@gmail.com>
2019-09-30 02:57:30 +0530

    maybe fix

--- apps/website/src/app/views/Home/Home.tsx
+++ apps/website/src/app/views/Home/Home.tsx
@@ -39,12 +39,13 @@ export default class Home extends Component<HomeProps & RouteComponentProps<URLP
   componentDidMount() {
     let { city } = this.props
     if(city===undefined) {
-      city = this.context.actions.getUserContext().city
-      if(city!==undefined)
+      try {
+        city = this.context.actions.getUserContext().city
         this.context.router(`/in/${city.toLowerCase()}`)
-      else
+      } catch (error) {
         this.context.router('/in/delhi')
-      // ^ open modal to select city here
+        // ^ open modal to select city here
+      }
     }
     else
       this.context.actions.setUserContext({
