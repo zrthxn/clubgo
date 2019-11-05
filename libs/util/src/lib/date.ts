6efd39ea3bf5065115f257a51cb527109122151d
Alisamar Husain <zrthxn@gmail.com>
2019-11-05 23:08:07 +0530

    anti dates

--- libs/util/src/lib/date.ts
+++ libs/util/src/lib/date.ts
@@ -39,13 +39,13 @@ export function getFormattedDate(_date:Date|number|string):DateFormat {
 }
 
 const antiDayOfTheWeek = {
-  'Monday': 0,
-  'Tuesday': 1,
-  'Wednesday': 2,
-  'Thursday': 3,
-  'Friday': 4,
-  'Saturday': 5,
-  'Sunday': 6
+  'Monday': 1,
+  'Tuesday': 2,
+  'Wednesday': 3,
+  'Thursday': 4,
+  'Friday': 5,
+  'Saturday': 6,
+  'Sunday': 7
 }
 
 const antiMonths = {
